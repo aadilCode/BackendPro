979ebd86b3eb0b33015c6cd991ea781d76640694
aadiCode <aadilahmed337@gmail.com>
2026-01-13 14:20:45 +0530

    error on apiError fixed

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -3,17 +3,17 @@ class apiError extends Error{
         statusCode,
         message = "something went wrong",
         errors = [],
-        statck = ""
+        stack = ""
     ){
         super(message)
         this.statusCode = statusCode
         this.data = null
-        this,message = message
+        this.message = message
         this.success = false;
         this.errors = errors 
 
-        if (statck) {
-            this.stack = statck
+        if (stack) {
+            this.stack = stack
         } else{
             Error.captureStackTrace(this, this.
             constructor)
